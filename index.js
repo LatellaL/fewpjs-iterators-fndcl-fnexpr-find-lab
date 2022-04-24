2ebbf2da64186c0e985ca18bd08dfdfca3f2c8d3
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-24 11:11:22 -0400

    commit tree a32b6d6f3e3a3e32debef5d6c90fe8c942e7ae9b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(array) {
+    let team = array.find(function(s) { return s.result === "W"} );
+    if(team) {
+        return team.year;
+    } else {
+        return team
+    }
+}
